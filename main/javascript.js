841de65ea8ca2c49d81a8ff5a8aba493f375f86a
Ary Farah <124756642+aryfarahs@users.noreply.github.com>
2024-05-28 00:56:53 -0300

    .

--- main/javascript.js
+++ main/javascript.js
@@ -1,3 +1,4 @@
+// CARROSSEL
 window.addEventListener('load', () => {
     handleSlider()
 })
@@ -41,4 +42,66 @@ function handleSlider() {
         })
     })
 
+}
+
+// --------------------------------------------------------------------------
+// Soft Scroll
+// const menuLinks = document.querySelectorAll('a[href^="#"]');
+
+// function getDistanceFromTheTop(element) {
+//     const id = element.getAttribute("href");
+//     return document.querySelector(id).offSetTop;
+// }
+
+// function nativeScroll(distanceFromTheTop) {
+//     window.scroll({
+//         top:distanceFromTheTop,
+//         behavior: "smooth"
+//     })
+// }
+
+// function scrollToSection(event) {
+//     event.preventDefault();
+//     const distanceFromTheTop = getDistanceFromTheTop(event.targer) - 90;
+//     nativeScroll(distanceFromTheTop);
+// }
+
+// menuLinks.forEach((link) => {
+//     link.addEventListener("click", scrollToSection);
+// });
+
+
+//-------------------------------------------------------------------
+// BOTAO FUJAO
+function fuja() {
+    var botaoNao = document.getElementById("nao");
+    var botoes = document.getElementById("amoMais");
+
+    var rect = botoes.getBoundingClientRect();
+    var larguraJanela = rect.width;
+    var alturaJanela = rect.height;
+
+    var maxX = larguraJanela - botaoNao.offsetWidth;
+    var maxY = alturaJanela - botaoNao.offsetHeight;
+
+    var aleatorioX = Math.floor(Math.random() * maxX);
+    var aleatorioY = Math.floor(Math.random() * maxY);
+
+    botaoNao.style.left = aleatorioX + "px";
+    botaoNao.style.top = aleatorioY + "px";
+}
+
+//-------------------------------------------------------------------
+// INVISIVEL
+function trocaDisplay() {
+    const display = document.getElementById("bomQueSabe");
+    const footer = document.getElementById("footer")
+
+    if (window.getComputedStyle(display).display === "none") {
+        display.style.display = "flex"; 
+        footer.style.marginTop = "620px"
+    } else {
+        display.style.display = "none";
+        footer.style.marginTop = "20px"
+    }
 }
